c08561c786ba8e9e06db74dd57c3082a66cb20b9
drzln <drzzln@protonmail.com>
2025-06-11 19:26:23 -0300

    init

--- lib/pangea-orchestrator.rb
+++ lib/pangea-orchestrator.rb
@@ -1,23 +1,5 @@
-# require 'pangea/config'
-# require 'pangea/state'
-# require 'pangea/utils'
-# require 'pangea/cli'
-# require 'json'
+# lib/pangea-orchestrator.rb
 
 module PangeaOrchestrator
-  # autoload :Module, File.join(__dir__, 'pangea', 'module')
-
-  # module App
-  #   class << self
-  #     def cfg
-  #       @cfg ||= Pangea::Utils.symbolize(
-  #         Pangea::Config.config
-  #       )
-  #     end
-  #
-  #     def run
-  #       Pangea::Cli.start(ARGV)
-  #     end
-  #   end
-  # end
+  autoload :Module, File.join(__dir__, 'pangea', 'module')
 end
