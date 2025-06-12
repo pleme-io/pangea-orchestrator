97f577ac46f1a7aa557fc95fdf53bb1ce52d1f8e
drzln <drzzln@protonmail.com>
2025-06-12 20:14:49 -0300

    init

---
+++ lib/pangea-orchestrator/resources/ecs.rb
@@ -0,0 +1,27 @@
+# lib/pangea-orchestrator/resources/ecs.rb
+def ecs(
+  name:,
+  # role_arn: nil,
+  # resource_name: nil,
+  # version: '1.31',
+  # access_config: { authentication_mode: :API },
+  # vpc_config: {},
+  # bootstrap_self_managed_addons: false,
+  # compute_config: {},
+  # kubernetes_network_config: {},
+  # storage_config: {}
+
+)
+  resource_name = name if resource_name.nil?
+  resource :aws_ecs_cluster, resource_name do
+    name name
+    # role_arn role_arn
+    # version version
+    # access_config access_config
+    # vpc_config vpc_config
+    # bootstrap_self_managed_addons bootstrap_self_managed_addons
+    # compute_config compute_config
+    # kubernetes_network_config kubernetes_network_config
+    # storage_config storage_config
+  end
+end
