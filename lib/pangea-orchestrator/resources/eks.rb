d3d15637ebafc13e51a816595e79aa3b753ade98
drzln <drzzln@protonmail.com>
2025-06-12 17:21:21 -0300

    init

--- lib/pangea-orchestrator/resources/eks.rb
+++ lib/pangea-orchestrator/resources/eks.rb
@@ -1,5 +1,16 @@
 # lib/pangea-orchestrator/resources/eks.rb
-def eks(name:, resource_name: nil)
+def eks(
+  name:,
+  resource_name: nil,
+  version: '1.31',
+  access_config: {authentication_mode: :API},
+  vpc_config: {},
+  bootstrap_self_managed_addons: false
+  compute_config: {}
+  kubernetes_network_config: {}
+  storage_config: {}
+
+)
   resource_name = name if resource_name.nil?
   resource :aws_eks_cluster, resource_name do
     name name
