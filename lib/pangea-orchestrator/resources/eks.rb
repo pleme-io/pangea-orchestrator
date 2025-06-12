4a12e50030d9d82219e097c81e9abed65c4c47b6
drzln <drzzln@protonmail.com>
2025-06-12 03:59:21 -0300

    init

--- lib/pangea-orchestrator/resources/eks.rb
+++ lib/pangea-orchestrator/resources/eks.rb
@@ -1,9 +1,15 @@
 # lib/pangea-orchestrator/resources/eks.rb
 require 'json'
+require 'terraform-synthesizer'
+
 module PangeaOrchestrator
   module Resources
     class EKS
       class << self
+        def synthesizer
+          @synthesizer || = Terraform::Synthesizer.new
+        end
+
         def symbolize(hash)
           JSON[JSON[hash, symbolic_names: true]]
         end
@@ -12,8 +18,10 @@ module PangeaOrchestrator
           kwargs        = symbolize(kwargs)
           resource_name = kwargs[:resource_name]
           name          = kwargs[:name]
-          resource :aws_eks_cluster, resource_name do
-            name name
+          synthesizer.synthesize do
+            resource :aws_eks_cluster, resource_name do
+              name name
+            end
           end
         end
       end
