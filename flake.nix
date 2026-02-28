fd48b57032d58b59a95e96ed158a087d47867159
drzzln <drzzln@protonmail.com>
2026-02-28 14:38:57 -0500

    chore: standardize nixpkgs to nixos-25.11
    Align with org-wide convention for binary cache consistency.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "orchestrator library";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   inputs.ruby-nix.url = "github:inscapist/ruby-nix";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
